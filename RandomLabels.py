ce9fd6b8bc68200003a0d51847a7db03bf52a96e
ruofeidu <ruofei@umiacs.umd.edu>
2017-12-13 16:54:42 -0500

    fixed rain.

--- RandomLabels.py
+++ RandomLabels.py
@@ -8,7 +8,7 @@ l_objects = {"common" : l_common, "group1" : l_g1, "group2" : l_g2}
 
 for obj, l in l_envs.items():
     for line in open("%s.txt" % obj):
-        l.append(" ".join(line.strip().split('\t')).lower())
+        l.append("\t".join(line.strip().split('\t')).lower())
     print(l)
 
 for obj, l in l_objects.items():
@@ -37,17 +37,19 @@ print("Total combinations: ", total)
 
 cnt = 0
 while cnt < num:
-    res = random.choice(l_wth) + " " + random.choice(l_env)
+    res = random.choice(l_wth) + "\t" + random.choice(l_env)
     x = random.randint(0, 2)
     if x == 1:
-        res += " " + random.choice(l_common)
+        res += "\t" + random.choice(l_common)
     elif x == 2:
-        res += " " + random.choice(l_common2)
+        res += "\t" + random.choice(l_common2)
     x = random.randint(2, 3)
     if x == 2:
-        res += " " + random.choice(l_g2)
+        res += "\t" + random.choice(l_g2)
     elif x == 3:
-        res += " " + random.choice(l_g3)
+        res += "\t" + random.choice(l_g3)
+    if res.count('\t') > 7:
+        continue
     if res not in hashset:
         hashset.add(res)
         cnt += 1
