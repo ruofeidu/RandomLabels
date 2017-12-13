e8d23dc0f8203d62b8d8a535d185e4c82d0d80d8
ruofeidu <ruofei@umiacs.umd.edu>
2017-12-13 15:50:04 -0500

    init

---
+++ RandomLabels.py
@@ -0,0 +1,11 @@
+
+l_weather = []
+for line in open("weather.txt"):
+    l_weather.append(line.strip().split('\t'))
+
+l_common = []
+
+for line in open("common.txt"):
+    object = line.strip()
+    l_common = 
+
